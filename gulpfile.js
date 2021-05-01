0a9044073e9a72d48f370357c0b54cb5abb4a2a0
nameistao <taoluo@usc.edu>
2021-05-01 11:53:01 -0700

    gulp command now watches and auto updated changes to front-end js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp');
 const terser = require('gulp-terser');
+const { watch, series } = require('gulp');
 
-function es(){
+function minifyJS(){
   return gulp.src('./public/js/*.js')
     .pipe(terser())
     .pipe(gulp.dest('./public/uglyJS'));
 }
 
-exports.default = es;
+//exports.default = es;
+
+exports.default = function() {
+  // You can use a single task
+  watch('public/js/*.js', minifyJS);
+};
