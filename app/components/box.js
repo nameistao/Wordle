1b3368bd05c756892a370d1d930be4a9d3b09914
tao.luo <taoluo@usc.edu>
2022-03-19 15:25:17 -0700

    update focus logic

--- app/components/box.js
+++ app/components/box.js
@@ -3,11 +3,17 @@ import { action } from '@ember/object';
 
 export default class BoxComponent extends Component {
   @action input(e) {
-    console.log(e.target.value);
+    console.log(e.target.nextElementSibling);
     if (e.target.value.match(/[a-z]/i)) {
       e.target.value = e.target.value.toUpperCase();
+      e.target.nextElementSibling.focus();
     } else {
       e.target.value = '';
     }
+  }
+
+  @action prev(e) {
+    e.target.value = '';
+    if (e.key === 'Backspace') e.target.previousElementSibling.focus();
   }
 }
